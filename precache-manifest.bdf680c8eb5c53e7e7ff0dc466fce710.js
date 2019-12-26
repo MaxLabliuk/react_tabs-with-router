ad142f8d361166496411919e24add26caac5a132
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2019-12-26 10:06:05 +0200

    Updates

---
+++ precache-manifest.bdf680c8eb5c53e7e7ff0dc466fce710.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da694444abcf6e74360b78f8ffa3f0f3",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "fdc7a3151b143628d5f8",
+    "url": "/react_tabs-with-router/static/css/main.3fa8964f.chunk.css"
+  },
+  {
+    "revision": "3eb6d0b8af5013e18824",
+    "url": "/react_tabs-with-router/static/js/2.cc1e8166.chunk.js"
+  },
+  {
+    "revision": "fdc7a3151b143628d5f8",
+    "url": "/react_tabs-with-router/static/js/main.06497b5f.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
